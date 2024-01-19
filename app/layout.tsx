a61a6de98ad26fe9d8f0b4a7de1232290e7e3cdf
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-01-19 20:55:02 +0600

    clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'NextEvent-Manager',
+  description: 'a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
