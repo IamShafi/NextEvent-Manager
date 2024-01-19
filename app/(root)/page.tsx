dde3c208b63c91452461860012c63da92d33aca5
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-01-19 21:21:49 +0600

    header section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,8 +24,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <h1 className="h1-bold">Elevate your events with our platform: Host, Connect, and Celebrate! !</h1>
+            <p className="p-regular-20 md:p-regular-24">Trusted by thousands of events worldwide.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
@@ -44,7 +44,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <h2 className="h2-bold">Proven Track Record
+at Countless Gatherings</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
